b0b641b272afbdc86280fc729e041f3300b2c835
shashidhar-sqilco <shashidhar@sqilco.com>
2024-10-11 16:53:22 +0530

    Day 5

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectTODb = require("./config/connectTODb");
 const userRouter = require("./controllers/UserController");
 const productsRoutes = require("./controllers/ProductsController");
@@ -24,9 +25,17 @@ app.use(cookieParser());
 app.use("/api/users", userRouter);
 app.use("/api/products", productsRoutes);
 
-//access controll 
+//access controll
 app.use("/api/cart", authMiddleware, cartRouter);
 app.use("/api/order", authMiddleware, orderRouter);
+
+// Serve static files from the React build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Handle any requests that don't match the API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 app.listen(8080, () => {
   console.log("server is running on port 8080");
