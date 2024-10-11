94bc8259e531c160d7a44310c426777214194606
shashidhar-sqilco <shashidhar@sqilco.com>
2024-10-11 17:18:59 +0530

    still trying...

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,9 @@ const dotenv = require("dotenv");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const authMiddleware = require("./middlewares/authMiddleware");
+const connectTODb = require("./config/connectTODb");
 
 dotenv.config();
-
-// Use dynamic imports for modules that might have path issues
-const connectTODb = import("./config/connectTODb.js").then(
-  (module) => module.default
-);
-const userRouter = import("./controllers/UserController.js").then(
-  (module) => module.default
-);
-const productsRoutes = import("./controllers/ProductsController.js").then(
-  (module) => module.default
-);
-const cartRouter = import("./controllers/CartController.js").then(
-  (module) => module.default
-);
-const orderRouter = import("./controllers/OrderController.js").then(
-  (module) => module.default
-);
 
 const app = express();
 
@@ -32,10 +16,17 @@ app.use(cookieParser());
 
 // Use async IIFE to set up routes after dynamic imports are resolved
 (async () => {
-  app.use("/api/users", await userRouter);
-  app.use("/api/products", await productsRoutes);
-  app.use("/api/cart", authMiddleware, await cartRouter);
-  app.use("/api/order", authMiddleware, await orderRouter);
+  const userRouter = (await import("./controllers/UserController.js")).default;
+  const productsRoutes = (await import("./controllers/ProductsController.js"))
+    .default;
+  const cartRouter = (await import("./controllers/CartController.js")).default;
+  const orderRouter = (await import("./controllers/OrderController.js"))
+    .default;
+
+  app.use("/api/users", userRouter);
+  app.use("/api/products", productsRoutes);
+  app.use("/api/cart", authMiddleware, cartRouter);
+  app.use("/api/order", authMiddleware, orderRouter);
 
   // Serve static files from the React build
   app.use(express.static(path.join(__dirname, "../frontend/build")));
@@ -50,8 +41,7 @@ app.use(cookieParser());
   app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
     try {
-      const db = await connectTODb;
-      await db();
+      await connectTODb();
       console.log("Connected to database");
     } catch (error) {
       console.error("Failed to connect to database:", error);
